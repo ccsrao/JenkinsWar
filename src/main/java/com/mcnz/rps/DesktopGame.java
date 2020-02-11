2a5433bd204d74c1c03e4083106d29cd12fadc74
ccsrao <chandrachinthapatla@gmail.com>
2020-02-11 16:32:57 +0530

    Update DesktopGame.java

--- src/main/java/com/mcnz/rps/DesktopGame.java
+++ src/main/java/com/mcnz/rps/DesktopGame.java
@@ -35,6 +35,7 @@ public class DesktopGame {
 			JOptionPane.showMessageDialog(null, result);
 			System.out.println(score);
 			for (GameSummary gameSummary : history) {
+				//test
 				System.out.println(gameSummary);
 			}
 			
